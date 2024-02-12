abeb5b31b32afb9c85d720e399f58b7c3ed33c1d
agasefamircan <agasefemircanov@gmail.com>
2024-02-12 21:31:41 +0400

    added tasks

---
+++ script.js
@@ -0,0 +1,23 @@
+// Task 1: Change the color of the greeting text
+document.getElementById("colorButton").addEventListener("click", function () {
+  // TODO: Complete the function to change the 'greeting' element's color to a random color
+  const greeting = document.getElementById("greeting");
+  greeting.style.color = getRandomColor();
+});
+
+// Task 2: Change the text content of the greeting
+document.getElementById("textButton").addEventListener("click", function () {
+  // TODO: Complete the function to change the text of the 'greeting' element to "Hello, JavaScript!"
+  const greeting = document.getElementById("greeting");
+  greeting.textContent = "Hello, JavaScript!";
+});
+
+// Helper function for generating a random color (Complete for the applicant)
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
